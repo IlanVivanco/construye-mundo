3648a1ee61f84ce17ec19a8efaa014ef6bfd7464
Ilán Vivanco <ilanvivanco@gmail.com>
2021-06-11 13:53:52 +0200

    Init child theme

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Construye Mundo Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Construye Mundo
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_VERSION', '1.0.0' );
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+	wp_enqueue_style( 'construye-mundo-theme-css', get_stylesheet_directory_uri() . '/style.css', array( 'astra-theme-css' ), CHILD_THEME_VERSION, 'all' );
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
