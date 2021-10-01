2fb0af14ab14999814513c5a218a1dfb88e3cbae
Ilán Vivanco <ilanvivanco@gmail.com>
2021-10-01 12:41:37 +0200

    Show only `noticias` category on the blog archive

--- functions.php
+++ functions.php
@@ -21,8 +21,17 @@ function iv_enqueue_styles() {
 	wp_enqueue_style( 'construye-mundo-theme-css', get_stylesheet_directory_uri() . '/style.css', array( 'astra-theme-css' ), CM_THEME_VERSION, 'all' );
 }
 
-function child_enqueue_styles() {
-	wp_enqueue_style( 'construye-mundo-theme-css', get_stylesheet_directory_uri() . '/style.css', array( 'astra-theme-css' ), CHILD_THEME_VERSION, 'all' );
+/**
+ * Filter blog page to show only 'noticias'
+ */
+add_action( 'pre_get_posts', 'iv_filter_blog_posts_on_blog_page' );
+
+function iv_filter_blog_posts_on_blog_page($query) {
+	if( !is_admin() && $query->is_home() && $query->is_main_query() ){
+		$query->set( 'cat', 3 );
+	}
+}
+
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
