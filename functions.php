757f2cc5f2f37d7b33e22f8aa6043424322ed76a
Ilán Vivanco <ilanvivanco@gmail.com>
2021-10-01 12:50:12 +0200

    Fix linting issues

--- functions.php
+++ functions.php
@@ -15,28 +15,28 @@ define( 'CM_THEME_VERSION', '1.0.0' );
 /**
  * Enqueue styles
  */
-add_action( 'wp_enqueue_scripts', 'iv_enqueue_styles', 15 );
-
 function iv_enqueue_styles() {
 	wp_enqueue_style( 'construye-mundo-theme-css', get_stylesheet_directory_uri() . '/style.css', array( 'astra-theme-css' ), CM_THEME_VERSION, 'all' );
 }
+add_action( 'wp_enqueue_scripts', 'iv_enqueue_styles', 15 );
 
 /**
  * Filter blog page to show only 'noticias'
+ *
+ * @param WP_Query $query Current $query.
  */
-add_action( 'pre_get_posts', 'iv_filter_blog_posts_on_blog_page' );
-
-function iv_filter_blog_posts_on_blog_page($query) {
-	if( !is_admin() && $query->is_home() && $query->is_main_query() ){
+function iv_filter_blog_posts_on_blog_page( $query ) {
+	if ( ! is_admin() && $query->is_home() && $query->is_main_query() ) {
 		$query->set( 'cat', 3 );
 	}
 }
+add_action( 'pre_get_posts', 'iv_filter_blog_posts_on_blog_page' );
 
 /**
  * Disables AddToAny's core script on non-posts
+ *
+ * @param Boolean $script_disabled  Current status.
  */
-add_filter( 'addtoany_script_disabled', 'addtoany_disable_script_except_for_posts' );
-
 function addtoany_disable_script_except_for_posts( $script_disabled ) {
 	if ( ! is_single() && ! is_archive() ) {
 		return true;
@@ -44,3 +44,6 @@ function addtoany_disable_script_except_for_posts( $script_disabled ) {
 
 	return $script_disabled;
 }
+add_filter( 'addtoany_script_disabled', 'addtoany_disable_script_except_for_posts' );
+
+
